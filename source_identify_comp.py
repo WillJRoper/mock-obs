d889dc8705824070336442a92b1b86aff7a5e24e
wjr21 <wjr21@sussex.ac.uk>
2023-05-08 16:03:25 +0100

    Correcting mass

--- source_identify_comp.py
+++ source_identify_comp.py
@@ -51,8 +51,8 @@ s_length = reg_snap_grp["Galaxy"]["S_Length"][...]
 s_begin = np.zeros(len(s_length), dtype=int)
 s_begin[1:] = np.cumsum(s_length[:-1])
 pos = reg_snap_grp["Particle"]["S_Coordinates"][...].T / (1 + z)
-s_mass = reg_snap_grp["Particle"]["S_Mass"][...] * 10 ** 10
-ini_masses = reg_snap_grp["Particle"]["S_MassInitial"][...] * 10 ** 10
+s_mass = reg_snap_grp["Particle"]["S_Mass"][...]
+ini_masses = reg_snap_grp["Particle"]["S_MassInitial"][...]
 s_mets = reg_snap_grp["Particle"]["S_Z"][...]
 ages = reg_snap_grp["Particle"]["S_Age"][...]
 los = reg_snap_grp["Particle"]["S_los"][...]

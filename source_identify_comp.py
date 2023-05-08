d58ee4f6291358c1a972e3daca6184542c2b6fdf
wjr21 <wjr21@sussex.ac.uk>
2023-05-08 16:26:08 +0100

    Actual resolution

--- source_identify_comp.py
+++ source_identify_comp.py
@@ -34,7 +34,7 @@ z_str = snap.split('z')[1].split('p')
 z = float(z_str[0] + '.' + z_str[1])
 
 # Define image properties
-resolution = 10 * 0.031 / cosmo.arcsec_per_kpc_proper(z).value * kpc
+resolution = 0.031 / cosmo.arcsec_per_kpc_proper(z).value * kpc
 width = 200 * kpc
 print("Making images with %.2f kpc resolution and a %.2f FOV" % (resolution, width))
 
@@ -193,13 +193,13 @@ ax4 = fig.add_subplot(224)
 
 # Plot images
 ax1.imshow(grp_mass_img, norm=mpl.colors.Normalize(
-    vmin=np.percentile(grp_mass_img[grp_mass_img > 0], 50),
+    vmin=np.percentile(grp_mass_img[grp_mass_img > 0], 32),
     vmax=np.percentile(grp_mass_img[grp_mass_img > 0], 99.5)),
            cmap="Greys_r"
            
            )
 ax2.imshow(grp_lum_img, norm=mpl.colors.Normalize(
-    vmin=np.percentile(grp_lum_img[grp_lum_img > 0], 50),
+    vmin=np.percentile(grp_lum_img[grp_lum_img > 0], 32),
     vmax=np.percentile(grp_lum_img[grp_lum_img > 0], 99.5)),
            cmap="Greys_r"
            )

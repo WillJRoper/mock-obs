118f0cf947060066b1740a3804f410e84258ddae
wjr21 <wjr21@sussex.ac.uk>
2023-05-22 22:34:10 +0100

    added individual component images

--- source_identify_comp.py
+++ source_identify_comp.py
@@ -40,6 +40,8 @@ snap = tags[snap_ind]
 z_str = snap.split('z')[1].split('p')
 z = float(z_str[0] + '.' + z_str[1])
 
+soft = 0.001802390 / (0.6777 * (1 + z)) 
+
 # Define image properties
 resolution = downsample * 0.031 / cosmo.arcsec_per_kpc_proper(z).value * kpc
 width = width * kpc
@@ -57,13 +59,26 @@ subgrps = reg_snap_grp["Galaxy"]["SubGroupNumber"][...]
 s_length = reg_snap_grp["Galaxy"]["S_Length"][...]
 s_begin = np.zeros(len(s_length), dtype=int)
 s_begin[1:] = np.cumsum(s_length[:-1])
-pos = reg_snap_grp["Particle"]["S_Coordinates"][...].T / (1 + z)
+s_pos = reg_snap_grp["Particle"]["S_Coordinates"][...].T / (1 + z)
 s_mass = reg_snap_grp["Particle"]["S_Mass"][...] * 10 ** 10
 ini_masses = reg_snap_grp["Particle"]["S_MassInitial"][...] * 10 ** 10
 s_mets = reg_snap_grp["Particle"]["S_Z"][...]
 ages = reg_snap_grp["Particle"]["S_Age"][...] * 10 ** 3
 los = reg_snap_grp["Particle"]["S_los"][...]
-smls =reg_snap_grp["Particle"]["S_sml"][...]
+s_smls = reg_snap_grp["Particle"]["S_sml"][...]
+
+dm_length = reg_snap_grp["Galaxy"]["DM_Length"][...]
+dm_begin = np.zeros(len(dm_length), dtype=int)
+dm_begin[1:] = np.cumsum(dm_length[:-1])
+dm_pos = reg_snap_grp["Particle"]["DM_Coordinates"][...].T / (1 + z)
+dm_mass = reg_snap_grp["Particle"]["DM_Mass"][...] * 10 ** 10
+
+g_length = reg_snap_grp["Galaxy"]["G_Length"][...]
+g_begin = np.zeros(len(g_length), dtype=int)
+g_begin[1:] = np.cumsum(g_length[:-1])
+g_pos = reg_snap_grp["Particle"]["G_Coordinates"][...].T / (1 + z)
+g_mass = reg_snap_grp["Particle"]["G_Mass"][...] * 10 ** 10
+g_smls = reg_snap_grp["Particle"]["G_sml"][...]
 
 print("Got data...")
 
@@ -72,36 +87,80 @@ okinds = grps == group_id
 grp_subgrps = subgrps[okinds]
 grp_s_length = s_length[okinds]
 grp_s_begin = s_begin[okinds]
+grp_dm_length = dm_length[okinds]
+grp_dm_begin = dm_begin[okinds]
+grp_g_length = g_length[okinds]
+grp_g_begin = g_begin[okinds]
 
 # And make particle arrays for this group
-grp_pos = []
+grp_s_pos = []
+grp_dm_pos = []
+grp_g_pos = []
 grp_s_mass = []
+grp_dm_mass = []
+grp_g_mass = []
 grp_ini_masses = []
 grp_s_mets = []
 grp_ages = []
 grp_los = []
-grp_smls = []
-subgrp_start = []
-subgrp_length = []
-for (ind, start), length in zip(enumerate(grp_s_begin), grp_s_length):
-    subgrp_start.append(len(grp_los))
-    subgrp_length.append(length)
-    grp_pos.extend(pos[start: start + length, :])
-    grp_s_mass.extend(s_mass[start: start + length])
-    grp_ini_masses.extend(ini_masses[start: start + length])
-    grp_s_mets.extend(s_mets[start: start + length])
-    grp_ages.extend(ages[start: start + length])
-    grp_los.extend(los[start: start + length])
-    grp_smls.extend(smls[start: start + length])
-grp_pos = np.array(grp_pos)
+grp_s_smls = []
+grp_dm_smls = []
+grp_g_smls = []
+subgrp_sstart = []
+subgrp_slength = []
+subgrp_dmstart = []
+subgrp_dmlength = []
+subgrp_gstart = []
+subgrp_glength = []
+for (ind, sstart), slength, dmstart, dmlength, gstart, glength in zip(
+        enumerate(grp_s_begin), grp_s_length,
+        grp_dm_begin, grp_dm_length,
+        grp_g_begin, grp_g_length):
+    
+    subgrp_sstart.append(len(grp_los))
+    subgrp_slength.append(slength)
+    subgrp_dmstart.append(len(grp_dm_mass))
+    subgrp_dmlength.append(dmlength)
+    subgrp_gstart.append(len(grp_g_mass))
+    subgrp_glength.append(glength)
+    
+    grp_s_pos.extend(s_pos[sstart: sstart + slength, :])
+    grp_s_mass.extend(s_mass[sstart: sstart + slength])
+    grp_ini_masses.extend(ini_masses[sstart: sstart + slength])
+    grp_s_mets.extend(s_mets[sstart: sstart + slength])
+    grp_ages.extend(ages[sstart: sstart + slength])
+    grp_los.extend(los[sstart: sstart + slength])
+    grp_s_smls.extend(s_smls[sstart: sstart + slength])
+
+    grp_dm_pos.extend(dm_pos[dmstart: dmstart + dmlength, :])
+    grp_dm_mass.extend(np.ones(dmlength))
+    grp_dm_smls.extend(np.full(dmlength, soft))
+    
+    grp_g_pos.extend(g_pos[gstart: gstart + glength, :])
+    grp_g_mass.extend(g_mass[gstart: gstart + glength])
+    grp_smls.extend(g_smls[gstart: gstart + glength])
+    
+grp_s_pos = np.array(grp_s_pos)
 grp_s_mass = np.array(grp_s_mass)
 grp_ini_masses = np.array(grp_ini_masses)
 grp_s_mets = np.array(grp_s_mets)
 grp_ages = np.array(grp_ages)
 grp_los = np.array(grp_los)
-grp_smls = np.array(grp_smls)
-subgrp_start = np.array(subgrp_start)
-subgrp_length = np.array(subgrp_length)
+grp_s_smls = np.array(grp_s_smls)
+subgrp_sstart = np.array(subgrp_sstart)
+subgrp_slength = np.array(subgrp_slength)
+
+subgrp_dmstart = np.array(subgrp_dmstart)
+subgrp_dmlength = np.array(subgrp_dmlength)
+grp_dm_pos = np.array(grp_dm_pos)
+grp_dm_mass = np.array(grp_dm_mass)
+grp_dm_smls = np.array(grp_dm_smls)
+
+subgrp_gstart = np.array(subgrp_gstart)
+subgrp_glength = np.array(subgrp_glength)
+grp_g_pos = np.array(grp_g_pos)
+grp_g_mass = np.array(grp_g_mass)
+grp_g_smls = np.array(grp_g_smls)
 
 print("Min and max masses:", grp_s_mass.min(), grp_s_mass.max(),
       grp_ini_masses.min(), grp_ini_masses.max())
@@ -114,6 +173,78 @@ print("Got the group data with %d particles" % len(grp_los))
 # Compute luminosities for this group
 lums = total_lum(grp_ini_masses, grp_s_mets, grp_ages, grp_los,
                  kappa=0.0795, BC_fac=1)
+
+# Get the group mass image
+grp_smass_obj = ParticleImage(
+    resolution,
+    fov=width,
+    cosmo=cosmo,
+    positions=grp_s_pos * Mpc,
+    pixel_values=grp_s_mass,
+    smoothing_lengths=grp_s_smls * Mpc,
+    rest_frame=True,
+    centre=centre
+)
+grp_smass_img = grp_smass_obj.get_smoothed_img(quintic)
+
+print("Got Stellar Mass Image", np.min(grp_mass_img[grp_mass_img > 0]),
+      np.max(grp_mass_img))
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.imshow(grp_smass_img)
+ax.axis('off')
+fig.savefig("plots/stellar/stellarmass_%s_%s_%d.png" % (snap, reg, group_id),
+            bbox_inches="tight", dpi=100, pad_inches=0)
+plt.close()
+
+# Get the group mass image
+grp_dmmass_obj = ParticleImage(
+    resolution,
+    fov=width,
+    cosmo=cosmo,
+    positions=grp_dm_pos * Mpc,
+    pixel_values=grp_dm_mass,
+    smoothing_lengths=grp_dm_smls * Mpc,
+    rest_frame=True,
+    centre=centre
+)
+grp_dmmass_img = grp_dmmass_obj.get_smoothed_img(quintic)
+
+print("Got Dark Matter Mass Image", np.min(grp_mass_img[grp_mass_img > 0]),
+      np.max(grp_mass_img))
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.imshow(grp_dmmass_img)
+ax.axis('off')
+fig.savefig("plots/darkmatter/darkmattermass_%s_%s_%d.png" % (snap, reg, group_id),
+            bbox_inches="tight", dpi=100, pad_inches=0)
+plt.close()
+
+# Get the group mass image
+grp_gmass_obj = ParticleImage(
+    resolution,
+    fov=width,
+    cosmo=cosmo,
+    positions=grp_g_pos * Mpc,
+    pixel_values=grp_g_mass,
+    smoothing_lengths=grp_g_smls * Mpc,
+    rest_frame=True,
+    centre=centre
+)
+grp_gmass_img = grp_gmass_obj.get_smoothed_img(quintic)
+
+print("Got Gas Mass Image", np.min(grp_mass_img[grp_mass_img > 0]),
+      np.max(grp_mass_img))
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.imshow(grp_gmass_img)
+ax.axis('off')
+fig.savefig("plots/gas/gasmass_%s_%s_%d.png" % (snap, reg, group_id),
+            bbox_inches="tight", dpi=100, pad_inches=0)
+plt.close()
 
 # Concert luminosity to flux
 lums = lum_to_flux(lums, cosmo, z)
@@ -132,9 +263,9 @@ grp_lum_obj = ParticleImage(
     resolution,
     fov=width,
     cosmo=cosmo,
-    positions=grp_pos * Mpc,
+    positions=grp_s_pos * Mpc,
     pixel_values=lums * nJy,
-    smoothing_lengths=grp_smls * Mpc,
+    smoothing_lengths=grp_s_smls * Mpc,
     psfs=psf,
     centre=centre,
     super_resolution_factor=2
@@ -146,22 +277,6 @@ grp_lum_img, grp_wht, grp_noise = grp_lum_obj.get_noisy_imgs(noise)
 
 print("Got Luminosity Image", np.min(grp_lum_img[grp_lum_img > 0]),
       np.max(grp_lum_img))
-
-# Get the group mass image
-grp_mass_obj = ParticleImage(
-    resolution,
-    fov=width,
-    cosmo=cosmo,
-    positions=grp_pos * Mpc,
-    pixel_values=grp_s_mass,
-    smoothing_lengths=grp_smls * Mpc,
-    rest_frame=True,
-    centre=centre
-)
-grp_mass_img = grp_mass_obj.get_smoothed_img(quintic)
-
-print("Got Mass Image", np.min(grp_mass_img[grp_mass_img > 0]),
-      np.max(grp_mass_img))
 
 # Set up colormap and normalisation
 norm = mpl.colors.Normalize(vmin=0, vmax=len(subgrp_start))

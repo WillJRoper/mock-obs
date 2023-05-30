460ed1cfe489405a95759e93fb87cf77f81b56f5
wjr21 <wjr21@sussex.ac.uk>
2023-05-30 19:52:16 +0100

    Correcting plot units

--- subgroup_img.py
+++ subgroup_img.py
@@ -223,7 +223,7 @@ for obj_id in object_ids:
     ax.loglog()
     ax.plot(int_sed.lam, int_sed._lnu)
     ax.set_ylim(10 ** 30.5, None)
-    ax.set_xlabel("$\lambda/ [\mu\mathrm{m}]$")
+    ax.set_xlabel("$\lambda/ [\AA]$")
     ax.set_ylabel("$L / [\mathrm{erg} / \mathrm{s} / \mathrm{Hz}]$")
     fig.savefig("plots/subgroup_%s_%s_%d_%d/spectra_luminosity.png" % (snap, reg, group_id, subgroup_id),
                 bbox_inches="tight", dpi=100, pad_inches=0)
@@ -233,7 +233,7 @@ for obj_id in object_ids:
     ax = fig.add_subplot(111)
     ax.loglog()
     ax.plot(int_sed.lamz, int_sed._fnu)
-    ax.set_xlabel("$\lambda/ [\mu\mathrm{m}]$")
+    ax.set_xlabel("$\lambda/ [\AA]$")
     ax.set_ylabel("$L / [\mathrm{nJy}]$")
     fig.savefig("plots/subgroup_%s_%s_%d_%d/spectra_flux.png" % (snap, reg, group_id, subgroup_id),
                 bbox_inches="tight", dpi=100, pad_inches=0)

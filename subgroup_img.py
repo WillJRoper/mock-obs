0878a179f2bc1ee10d1826071dc2b8223988b6d9
wjr21 <wjr21@sussex.ac.uk>
2023-06-08 17:59:58 +0100

    Testing PSF

--- subgroup_img.py
+++ subgroup_img.py
@@ -79,6 +79,9 @@ for f in rest_filters.filter_codes:
     nc = webbpsf.NIRCam()
     nc.filter = f.split(".")[-1]
     psfs[f] = nc.calc_psf(oversample=4)[0].data
+    print(psfs[f].max())
+    psfs[f] = psfs[f] / np.sum(psfs[f])
+    print(psfs[f].max())
 
 print("Got the PSFs")
 
